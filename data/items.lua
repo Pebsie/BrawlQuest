122dacf240de345ddc824c0c4586b85b89eade27
Thomas Lock <email@peb.si>
2017-08-29 14:46:37 +0100

    add player graphics

--- data/items.lua
+++ data/items.lua
@@ -70,119 +70,122 @@ function initItems()
   item.val[ti] = 0
   item.lvl[ti] = 1
   item.price[ti] = 0
-  item.img[ti] = ""
+  item.img[ti] = love.graphics.newImage("img/human/Adventurer.png")
 
   local ti = "Basic Cloth"
   item.type[ti] = "arm"
   item.val[ti] = 1
   item.lvl[ti] = 1
   item.price[ti] = 2
-  item.img[ti] = ""
+  item.img[ti] = love.graphics.newImage("img/human/Clean Adventurer.png")
 
   local ti = "Basic Leather"
   item.type[ti] = "arm"
   item.val[ti] = 2
   item.lvl[ti] = 2
   item.price[ti] = 5
-  item.img[ti] = ""
+  item.img[ti] = love.graphics.newImage("img/human/Fancy Adventurer.png")
 
   local ti = "Leather Armour"
   item.type[ti] = "arm"
   item.val[ti] = 3
   item.lvl[ti] = 3
   item.price[ti] = 15
-  item.img[ti] = ""
+  item.img[ti] = love.graphics.newImage("img/human/Clothed Adventurer.png")
 
   local ti = "Basic Chain"
   item.type[ti] = "arm"
   item.val[ti] = 5
   item.lvl[ti] = 5
   item.price[ti] = 25
-  item.img[ti] = ""
+  item.img[ti] = love.graphics.newImage("img/human/Soldier.png")
 
   local ti = "Chainmail Armour"
   item.type[ti] = "arm"
   item.val[ti] = 8
   item.lvl[ti] = 7
   item.price[ti] = 50
-  item.img[ti] = ""
+  item.img[ti] = love.graphics.newImage("img/human/Warrior.png")
 
   local ti = "Masterwork Chainmail Armour"
   item.type[ti] = "arm"
   item.val[ti] = 10
   item.lvl[ti] = 9
   item.price[ti] = 75
-  item.img[ti] = ""
-
-  local ti = "Steel Armour"
-  item.type[ti] = "arm"
-  item.val[ti] = 12
-  item.lvl[ti] = 10
-  item.price[ti] = 100
-  item.img[ti] = ""
+  item.img[ti] = love.graphics.newImage("img/human/Master Warrior.png")
 
   local ti = "Exotic Masterwork Chainmail Armour"
   item.type[ti] = "arm"
   item.val[ti] = 15
   item.lvl[ti] = 11
   item.price[ti] = 200
-  item.img[ti] = ""
-
-  local ti = "Masterwork Steel Armour"
-  item.type[ti] = "arm"
-  item.val[ti] =  17
-  item.lvl[ti] = 11
-  item.price[ti] = 200
-  item.img[ti] = ""
-
-  local ti = "Exotic Masterwork Steel Armour"
-  item.type[ti] = "arm"
-  item.val[ti] = 20
-  item.lvl[ti] = 12
-  item.price[ti] = 200
-  item.img[ti] = ""
+  item.img[ti] = love.graphics.newImage("img/human/Exotic Warrior.png")
 
   local ti = "Ascended Masterwork Chainmail Armour"
   item.type[ti] = "arm"
   item.val[ti] = 22
   item.lvl[ti] = 12
   item.price[ti] = 200
-  item.img[ti] = ""
+  item.img[ti] = love.graphics.newImage("img/human/Ascended Warrior.png")
+
+
+  local ti = "Steel Armour"
+  item.type[ti] = "arm"
+  item.val[ti] = 12
+  item.lvl[ti] = 10
+  item.price[ti] = 100
+  item.img[ti] = love.graphics.newImage("img/human/Champion.png")
+
+  local ti = "Masterwork Steel Armour"
+  item.type[ti] = "arm"
+  item.val[ti] =  17
+  item.lvl[ti] = 11
+  item.price[ti] = 200
+  item.img[ti] = love.graphics.newImage("img/human/Master Champion.png")
+
+  local ti = "Exotic Masterwork Steel Armour"
+  item.type[ti] = "arm"
+  item.val[ti] = 20
+  item.lvl[ti] = 12
+  item.price[ti] = 200
+  item.img[ti] = love.graphics.newImage("img/human/Exotic Champion.png")
+
 
   local ti = "Ascended Masterwork Steel Armour"
   item.type[ti] = "arm"
   item.val[ti] = 25
   item.lvl[ti] = 12
   item.price[ti] = 200
-  item.img[ti] = ""
+  item.img[ti] = love.graphics.newImage("img/human/Ascended Champion.png")
 
   local ti = "Guardian's Padding"
   item.type[ti] = "arm"
   item.val[ti] = 30
   item.lvl[ti] = 13
   item.price[ti] = 210
-  item.img[ti] = ""
+  item.img[ti] = love.graphics.newImage("img/human/Guardian.png")
 
   local ti = "King's Padding"
   item.type[ti] = "arm"
   item.val[ti] = 32
   item.lvl[ti] = 13
   item.price[ti] = 210
-  item.img[ti] = ""
+  item.img[ti] = love.graphics.newImage("img/human/King.png")
 
   local ti = "Legacy of Forest Heroes"
   item.type[ti] = "arm"
   item.val[ti] = 50
   item.lvl[ti] = 15
   item.price[ti] = 220
-  item.img[ti] = ""
+  item.img[ti] = love.graphics.newImage("img/human/Forest Hero.png")
 
     --legendary armour
     local ti = "Legendary Padding"
     item.type[ti] = "arm"
-    item.val[ti] = 50
+    item.val[ti] = 55
     item.lvl[ti] = 15
     item.price[ti] = 500
+    item.img[ti] = love.graphics.newImage("img/human/Legend.png")
 
   --consumables
   local ti = "Weak Red Potion"
