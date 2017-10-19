8f8699e131f1713590127cee530a34a2252308a4
Thomas Lock <email@peb.si>
2017-10-19 17:25:52 +0100

    allow players to equip weapons and armor

--- server/modules/players.lua
+++ server/modules/players.lua
@@ -41,13 +41,13 @@ function newPlayer(name, password)
   pl.t[i] = 9457 --CHANGE TO STARTING ZONE WHEN MAP IS READY <=== I've done that tyvm :)
   pl.wep[i] = "Long Stick"
   pl.arm[i] = "Old Cloth"
-  pl.inv[i] = "None"
+  pl.inv[i] = "Legendary Blade;1"
   pl.pot[i] = "Red Potion"
   pl.lvl[i] = 1
   pl.xp[i] = 0
   pl.at[i] = false
   pl.atm[i] = 0
-  pl.msg[i] = "chat|Server|Welcome "..i.."!|"
+  pl.msg[i] = "world"
   pl.online[i] = true
   pl.state[i] = "world"
 
@@ -57,7 +57,7 @@ end
 function updatePlayers(dt)
   for i = 1, countPlayers() do
     local k = getPlayerName(i)
-    
+
     pl.atm[k] = pl.atm[k] - 1*dt
     if pl.atm[k] < 0 then pl.at[k] = false end
 
@@ -100,25 +100,77 @@ function givePlayerGold(name, gold)
   pl.gold[name] = pl.gold[name] + gold
 end
 
-function givePlayerItem(name, item, amount)
+function givePlayerItem(name, ritem, amount)
   if not amount then amount = 1 end
 
   curInv = atComma(pl.inv[name], ";")
   local alreadyOwned = false
 
   for i=1,#curInv,2 do
-    if curInv[i] == item then curInv[i+1] = curInv[i+1] + amount alreadyOwned = true end
+    curInv[i+1] = tonumber(curInv[i+1])
+    if curInv[i] == ritem then curInv[i+1] = curInv[i+1] + amount alreadyOwned = true addMsg(name.." obtained another "..amount.."x "..ritem) end
   end
 
   if alreadyOwned == false then
-    curInv[#curInv + 1] = item
+    curInv[#curInv + 1] = ritem
     curInv[#curInv + 1] = amount
+    addMsg(name.." obtained "..amount.."x "..ritem)
   end
+
+
 
   pl.inv[name] = ""
   --rebuild inventory string
-  for i = 1, curInv[#curInv] do
-    pl.inv[name] = curInv[i]..";"
+  for i = 1, #curInv do
+    addMsg(curInv[i].." ("..i.."/"..#curInv..")")
+    pl.inv[name] = pl.inv[name]..curInv[i]..";"
+    addMsg(pl.inv[name])
+  end
+end
+
+function playerUse(name, ritem)
+  curInv = atComma(pl.inv[name],";")
+  local hasItem = false
+
+  rebuiltInv = {}
+  local k = 1
+
+  for i = 1, #curInv, 2 do
+    curInv[i+1] = tonumber(curInv[i+1])
+    if curInv[i] == ritem then --found item in inventory
+      hasItem = true
+      curInv[i+1] = curInv[i+1] - 1
+    end
+
+    rebuiltInv[k] = {}
+    rebuiltInv[k].item = curInv[i]
+    rebuiltInv[k].amount = curInv[i+1]
+    k = k + 1
+  end
+
+  --use item
+  if hasItem == true then
+    if item.type[ritem] == "wep" then
+      rebuiltInv[#rebuiltInv + 1] = {}
+      rebuiltInv[#rebuiltInv].item = pl.wep[name]
+      rebuiltInv[#rebuiltInv].amount = 1
+      pl.wep[name] = ritem
+    elseif item.type[ritem] == "arm" then
+      rebuiltInv[#rebuiltInv + 1] = {}
+      rebuiltInv[#rebuiltInv].item = pl.arm[name]
+      rebuiltInv[#rebuiltInv].amount = 1
+      pl.arm[name] = ritem
+    end
+  else
+    addMsg(name.." tried to use an item ("..ritem..") that they don't own!")
+  end
+
+  --rebuild inventory
+  pl.inv[name] = ""
+  for i = 1, #rebuiltInv do
+    if rebuiltInv[i].amount > 0 then
+      givePlayerItem(name, rebuiltInv[i].item, rebuiltInv[i].amount)
+    end
   end
 end
 
